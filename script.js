2dd049e5c4188fb8fe962d568f1942f9fc23116a
vic276ky <vignesh.25ec@licet.ac.in>
2026-08-16 22:03:03 +0530

    header changes

--- script.js
+++ script.js
@@ -12,6 +12,48 @@ const text = "Cybersecurity Enthusiast | Engineer";
 const typingElement = document.querySelector('.typing');
 let index = 0;
 
+const name = "VIGNESH R";
+const leetMap = {
+    'I': '1',
+	'G': '9',
+    'E': '3',
+    'S': '5'
+};
+
+const nameElement = document.querySelector('h1');
+
+function leetEffect() {
+    let nameArray = name.split('');
+    let index = 0;
+
+    function transformNext() {
+        if (index < nameArray.length) {
+            if (leetMap[nameArray[index]]) {
+                let temp = nameArray.slice();
+                temp[index] = leetMap[nameArray[index]];
+                nameElement.textContent = temp.join('');
+                
+                setTimeout(() => {
+                    nameElement.textContent = nameArray.join('');
+                    index++;
+                    setTimeout(transformNext, 300);
+                }, 400);
+            } else {
+                index++;
+                setTimeout(transformNext, 300);
+            }
+        }
+    }
+    transformNext();
+}
+
+function repeatLeet() {
+    leetEffect();
+    setTimeout(repeatLeet, name.length * 500 + 100);
+}
+
+repeatLeet();
+
 function type() {
     if (index < text.length) {
         typingElement.textContent += text[index];
